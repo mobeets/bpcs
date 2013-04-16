7557131ddce138651c7286808e4d48e636d76ab1
Jay H <mobeets@gmail.com>
2013-04-16 18:05:40 +1000

    wrote bin-to-decimal fcns for eventual bit_planing

--- array_bit_plane.py
+++ array_bit_plane.py
@@ -7,6 +7,22 @@ class BitPlane:
     def __init__(self, arr):
         self.arr = arr
 
+    def bin_strs_to_decimal(self, vals):
+        """
+        vals is list of int, each val either 0 or 1
+        returns the decimal value from concatenating vals
+        """
+        return int(''.join([str(x) for x in vals]),2)
+
+    def decimal_to_bin_strs(self, val, nbits):
+        """
+        val is int
+        nbits is int
+        returns val as list of int, each either 0 or 1,
+            representing val in base-2 using nbits
+        """
+        return [int(x) for x in bin(val)[2:].zfill(nbits)]
+
     def slice(self, nbits):
         """
         converts the values in the last dimension of self.arr into binary
