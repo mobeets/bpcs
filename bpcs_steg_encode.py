ff2e34441ee199823ae751644964fb38cd54c3d7
Jay H <mobeets@gmail.com>
2013-04-27 15:40:02 +1000

    adding histogram to show complexity of image
    Former-commit-id: 61550e59ba662f6da69f606d474dec1e5b75a558

--- bpcs_steg_encode.py
+++ bpcs_steg_encode.py
@@ -58,8 +58,8 @@ def embed_message_in_vessel(arr, alpha, message, grid_size):
 class BPCSEncodeImage(ActOnImage):
     def modify(self, messagefile, alpha):
         new_arr = np.array(self.arr, copy=True)
-        read_message_grids(messagefile, (8,8))
-        return embed_message_in_vessel(new_arr, alpha, message, (8,8))
+        message_grids = read_message_grids(messagefile, (8,8))
+        return embed_message_in_vessel(new_arr, alpha, message_grids, (8,8))
 
 def bpcs_steg_encode(infile, messagefile, outfile, alpha):
     BPCSEncodeImage(infile, as_rgb=True, bitplane=True, gray=True, nbits_per_layer=8)
@@ -67,7 +67,7 @@ def bpcs_steg_encode(infile, messagefile, outfile, alpha):
     x.write(outfile, arr)
 
 if __name__ == '__main__':
-    infile = 'docs/vessel_small.png'
+    infile = 'docs/vessel_mini.png'
     messagefile = 'docs/message.txt'
     outfile = infile.replace('.', '_encoded.')
     alpha 0.45
--- complexity_flippers.py
+++ complexity_flippers.py
@@ -7,19 +7,18 @@ from act_on_image import ActOnImage
 from bpcs_steg import arr_bpcs_complexity, conjugate
 from array_grid import get_next_grid_dims
 
-def histogram_of_complexities(arr1, arr2, params):
+def histogram_of_complexity(arr, grid_size):
     log.critical('Creating histograms of image complexity...')
-    vals1 = [arr_bpcs_complexity(arr1[dims]) for dims in get_next_grid_dims(arr1, params.grid_size)]
-    vals2 = [arr_bpcs_complexity(arr2[dims]) for dims in get_next_grid_dims(arr2, params.grid_size)]
+    vals = [arr_bpcs_complexity(arr[dims]) for dims in get_next_grid_dims(arr, grid_size)]
     fig = plt.figure()
     ax = fig.add_subplot(211)
-    n, bins, patches = ax.hist(vals1, 200, facecolor='red', alpha=0.75)
-    ax = fig.add_subplot(212)
-    n, bins, patches = ax.hist(vals2, 200, facecolor='red', alpha=0.75)
-    plt.show()
+    n, bins, patches = ax.hist(vals, 200, facecolor='red', alpha=0.75)
+    # ax = fig.add_subplot(212)
+    # n, bins, patches = ax.hist(vals2, 200, facecolor='red', alpha=0.75)
+    # plt.show()
+    return fig
 
 def flip_image_complexity(arr, alpha, comp_fcn, grid_size):
-    # arr1 = np.copy(arr)
     n = 0
     for dims in get_next_grid_dims(arr, grid_size):
         grid = arr[dims]
@@ -30,8 +29,12 @@ def flip_image_complexity(arr, alpha, comp_fcn, grid_size):
             assert abs((1 - arr_bpcs_complexity(init_grid)) - arr_bpcs_complexity(grid)) < 0.01
             assert not(arr[dims].tolist() == init_grid.tolist() and alpha != 0.5)
     log.critical('Conjugated {0} grids'.format(n))
-    # histogram_of_complexities(arr1, arr, params)
+    # histogram_of_complexity(arr, params)
     return arr, n
+
+class HistogramComplexityImage(ActOnImage):
+    def modify(self):
+        return histogram_of_complexity(self.arr, (8,8))
 
 class ComplexifyImage(ActOnImage):
     def modify(self, alpha):
@@ -42,6 +45,12 @@ class SimplifyImage(ActOnImage):
     def modify(self, alpha):
         new_arr = np.array(self.arr, copy=True)
         return flip_image_complexity(new_arr, alpha, lambda x,thresh: x<thresh, (8,8))
+
+def histogram(infile, outfile):
+    x = HistogramComplexityImage(infile, as_rgb=True, bitplane=True, gray=True, nbits_per_layer=8)
+    hist = x.modify()
+    hist.savefig(outfile)
+    plt.show()
 
 def complexify(infile, outfile, alpha):
     x = ComplexifyImage(infile, as_rgb=True, bitplane=True, gray=True, nbits_per_layer=8)
@@ -54,3 +63,8 @@ def simplify(infile, outfile, alpha):
     arr, stats = x.modify(alpha)
     x.write(outfile, arr)
     return stats
+
+if __name__ == '__main__':
+    infile = 'docs/vessel_small.png'
+    outfile = infile.replace('.png', '_complexity_hist.png')
+    histogram(infile, outfile)
