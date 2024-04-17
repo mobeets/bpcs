489d56fede2599d86e046a4c4a44fd102e22ebba
Dominik Łukasiewicz <dominikluk2@gmail.com>
2024-04-17 15:53:00 +0200

    Renamed parameter

--- bpcs/bpcs_steg_encode.py
+++ bpcs/bpcs_steg_encode.py
@@ -86,15 +86,15 @@ class BPCSEncodeImage(ActOnImage):
         message_grids = read_message_grids(messagefile, (8,8))
         return embed_message_in_vessel(new_arr, alpha, message_grids, (8,8))
 
-def encode(infile, messagefile, outfile, alpha=0.45, outbitplatefile=''):
+def encode(infile, messagefile, outfile, alpha=0.45, outbitplatedir=''):
     x = BPCSEncodeImage(infile, as_rgb=True, bitplane=True, gray=True, nbits_per_layer=8)
     arr_beforeModify = x.arr
     arr = x.modify(messagefile, alpha)
     x.write(outfile, arr)
-    if outbitplatefile != '':
+    if outbitplatedir != '':
         try:
-            if not os.path.exists(outbitplatefile):
-                os.makedirs(outbitplatefile)
+            if not os.path.exists(outbitplatedir):
+                os.makedirs(outbitplatedir)
         except:
-            raise Exception("The path provided is not the path to the folder: {0}".format(outbitplatefile))            
-        x.writeBitplate(outbitplatefile, arr_beforeModify, arr)
+            raise Exception("The path provided is not the path to the folder: {0}".format(outbitplatedir))            
+        x.writeBitplate(outbitplatedir, arr_beforeModify, arr)
